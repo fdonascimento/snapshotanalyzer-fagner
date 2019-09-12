09fc2dcbbb393a00d168f510f0966e48998b9882
Fagner Fonseca <fagneolli@gmail.com>
2019-09-12 08:30:20 -0300

    Filter instances by name. Also, stop and start instances.

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -4,17 +4,63 @@ import click
 session = boto3.Session(profile_name='default')
 ec2 = session.resource('ec2')
 
-@click.command()
-def list_intances():
+def filter_instances(name):
+    instances = []
+
+    if name:
+        filters = [{'Name':'tag:Name', 'Values':[name]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    return instances
+
+@click.group()
+def instances():
+    """Commands for instances"""
+
+@instances.command('list')
+@click.option('--name', default=None,
+    help="Only instances for name (tag Name:<name>)")
+def list_intances(name):
     "List EC2 instances"
-    for i in ec2.instances.all():
+    instances = filter_instances(name)
+
+    for i in instances:
+        tags = {t['Key']: t['Value'] for t in i.tags or []}
         print(', '.join((
             i.id,
             i.instance_type,
             i.placement['AvailabilityZone'],
             i.state['Name'],
-            i.public_dns_name
+            i.public_dns_name,
+            tags.get('Name', '<no name>')
         )))
+    return
+
+@instances.command('stop')
+@click.option('--name', default=None,
+    help='Only instances for name')
+def stop_instances(name):
+    "Stop EC2 instances"
+    instances = filter_instances(name)
+
+    for i in instances:
+        print("Stopping {0}...".format(i.id))
+        i.stop()
+    return
+
+@instances.command('start')
+@click.option('--name', default=None,
+    help='Only instances for name')
+def start_instances(name):
+    "Start EC2 instances"
+    instances = filter_instances(name)
+
+    for i in instances:
+        print("Starting {0}...".format(i.id))
+        i.start()
+    return
 
 if __name__ =='__main__':
-    list_intances()
+    instances()
